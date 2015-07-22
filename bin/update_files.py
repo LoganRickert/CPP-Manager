38d089bf74329c2d2730cc89df9702df93b343f5
Logan Rickert <logan@rickertcorp.com>
2015-07-22 01:20:13 -0400

    Removes redundent #includes and @version/@updated works again

--- bin/update_files.py
+++ bin/update_files.py
@@ -1,12 +1,18 @@
 
 import sys
 import datetime
+import operator
 
 operation = sys.argv[1]
 file_path = sys.argv[2]
 
 now = datetime.datetime.now()
 date = now.strftime("%m-%d-%Y %H:%M:%S")
+
+def sortByColumn(A,*args):
+    import operator
+    A.sort(key=operator.itemgetter(*args))
+    return A
 
 def build_inc():
 	build_file = None
@@ -16,7 +22,18 @@ def build_inc():
 
 	build_file = build_file.split('\n')
 
+	loc_includes = {}
+	loc_includes_lines = []
+	ext_includes = {}
+	ext_includes_lines = []
+
 	for i, line in enumerate(build_file):
+		if "#include <" in line:
+			ext_includes[line] = i
+			ext_includes_lines.append(i)
+		if "#include \"" in line:
+			loc_includes[line] = i
+			loc_includes_lines.append(i)
 		if "@version" in line:
 			temp_line = build_file[i].split(' ')
 			version = temp_line[1].split('.')
@@ -27,6 +44,33 @@ def build_inc():
 		
 		if "@updated" in line:
 			build_file[i] = "\t@updated " + date
+
+	loc_includes = loc_includes.keys()
+	ext_includes = ext_includes.keys()
+
+	loc_includes.sort()
+	loc_includes_lines.sort()
+	ext_includes.sort()
+	ext_includes_lines.sort()
+
+	countA = 0
+	for i, include in enumerate(loc_includes):
+		build_file[loc_includes_lines[i]] = include
+		countA = i
+
+	countB = 0
+	for i, include in enumerate(ext_includes):
+		build_file[ext_includes_lines[i]] = include
+		countB = i
+
+	for i in loc_includes_lines[countA + 1:]:
+		build_file[i] = "// REMOVE THIS LINE"
+
+	for i in ext_includes_lines[countB + 1:]:
+		build_file[i] = "// REMOVE THIS LINE"
+
+	while "// REMOVE THIS LINE" in build_file: 
+		build_file.remove("// REMOVE THIS LINE")   
 
 	with open(file_path, 'w') as f:
 		f.write('\n'.join(build_file))
